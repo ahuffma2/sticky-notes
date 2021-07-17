973f0db5c834a7b199455b394569811aa70a699c
Austin Huffman <austinhuffman0@gmail.com>
2021-07-17 13:24:06 -0400

    Working on getting API calls

--- routes/noteApiRoute.js
+++ routes/noteApiRoute.js
@@ -5,8 +5,54 @@ const path = require('path');
 const uuid = require('uuid');
 
 
-// router.get('/api/notes', (req,res) => {
-//     res.sendFile(path.join(__dirname, './public/notes.html'))
-// })
+const fs = require('fs');
+let notes;
+
+// var obj;
+// fs.readFile('file', 'utf8', function (err, data) {
+//   if (err) throw err;
+//   obj = JSON.parse(data);
+// });
+
+
+// Read the file and send to the callback
+
+
+// Write the callback function
+function handleFile(err, notes) {
+    if (err) throw err
+    notes = JSON.parse(notes)
+    console.log(notes);
+    return notes;
+}
+
+router.get('/notes', (req,res,) => {
+    console.log('I am getting notes...');
+    fs.readFile(path.join(__dirname,'../db/db.json'), handleFile)
+   // console.log(;
+    res.end;
+});
+
+// getNoteFile = () => {
+//     JSON.parse(fs.readFileSync('../db/db.json','utf8'));
+// }
+// router.post('./api/notes',(req,res) => res.send)
+
+
+
+//   app.post('/api/notes', (req,res) => {
+//      const newNote = req.body;
+
+//     //NEED TO SET A newNote ID IF I WANT TO DELETE
+//      console.log(`I want to add the note ${JSON.stringify(newNote)}`);
+//      addNote(JSON.stringify(newNote));
+//    //   res.json());
+//  });
+
+//  function addNote (note) {
+//     fs.writeFile('./db/db.json',note, (err) => 
+//     err ? console.log(err) : console.log('Success!')
+//   );
+//  }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 // const path = require('path');  //MIGHT NOT NEED THIS
-const uuid = require('uuid');
+//const uuid = require('uuid');
 const fs = require('fs');
 
 const htmlRoute = require('./routes/pageRoute');
+const apiRoute = require('./routes/noteApiRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,27 +14,6 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/',htmlRoute);
-
-
-
-
-// // app.get('*',(req,res) => res.sendFile(path.join(__dirname, './public/index.html')));
-//  app.get('/api/notes/', (req,res) => res.sendFile.json(path.join(__dirname, './db/db.json')));
-
-
-//   app.post('/api/notes', (req,res) => {
-//      const newNote = req.body;
-
-//     //NEED TO SET A newNote ID IF I WANT TO DELETE
-//      console.log(`I want to add the note ${JSON.stringify(newNote)}`);
-//      addNote(JSON.stringify(newNote));
-//    //   res.json());
-//  });
-
-//  function addNote (note) {
-//     fs.writeFile('./db/db.json',note, (err) => 
-//     err ? console.log(err) : console.log('Success!')
-//   );
-//  }
+app.use('/api',apiRoute);
 
 app.listen(PORT, () => console.log(`App listen on port ${PORT}`));
