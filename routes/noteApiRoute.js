31dd9e618ddbacd35e0aba5906d3a91c8a892b81
Austin Huffman <austinhuffman0@gmail.com>
2021-07-17 18:11:25 -0400

    ID for note to be deleted is specified

--- routes/noteApiRoute.js
+++ routes/noteApiRoute.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const fs = require('fs')
 
-const noteDB = require('../db/db.json');
+let noteDB = require('../db/db.json');
 const { randomUUID } = require('crypto');
 
 
@@ -13,57 +13,21 @@ router.get('/notes', (req,res) =>  {
 })
 
 router.post('/notes', (req,res) => {
-
-    
-    // If you want the file to be valid JSON, you have to open your file, parse the JSON, 
-    // append your new result to the array, 
-    // transform it back into a string and save it again.
     let newNote = req.body;
     newNote.id = randomUUID();
     noteDB.push(newNote);
     res.send(newNote);
 });
 
-// router.delete('/notes:id',(req,res) => {
-//     const id = req.params;
-//     console.log(id);
-//     res.end();
-// });
+router.delete('/notes/:id',(req,res) => {
+    const id = req.params; //THIS IS GOOD DONT CHANGE
+    let newNoteDB = noteDB;
+    console.log('I am the new Note DB' + newNoteDB);
 
-
-//CRUD FUNCTIONALITY
-
-// Write the callback function
-// function handleFile(err, notes) {
-//     if (err) throw err
-    
-//     console.log(notes);
-//     let noteList = [];
-//     noteList.concat(JSON.parse(notes));
-//     return noteList;
-// }
-
-
-// getNoteFile = () => 
-//     JSON.parse(fs.readFileSync('../db/db.json','utf8'));
-// }
-// router.post('./api/notes',(req,res) => res.send)
-
-
-
-//   app.post('/api/notes', (req,res) => {
-//      const newNote = req.body;
-
-//     //NEED TO SET A newNote ID IF I WANT TO DELETE
-//      console.log(`I want to add the note ${JSON.stringify(newNote)}`);
-//      addNote(JSON.stringify(newNote));
-//    //   res.json());
-//  });
-
-//  function addNote (note) {
-//     fs.writeFile('./db/db.json',note, (err) => 
-//     err ? console.log(err) : console.log('Success!')
-//   );
-//  }
+     console.log(newNoteDB.filter((note) => {
+         newNoteDB = note.id !== '1';
+     }))
+    res.end();
+});
 
 module.exports = router;
